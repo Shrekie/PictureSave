21d8797b79b82a46abb8803973cd2762d2221d65
Shrekie <thomaslindauer@mail.com>
2017-07-06 06:25:21 +0200

    dirname heroku variable

--- server/server.js
+++ server/server.js
@@ -111,9 +111,9 @@ app.get('/categories', (req, res) =>{
 
 // });
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(process.env.PWD || __dirname + '/public/'));
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(process.env.PWD || __dirname + '/public/index.html');
 })
 
 app.listen(process.env.PORT, () => {
